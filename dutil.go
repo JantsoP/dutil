235dbd4c102ac3c47481f0f8c402883d39ff129e
jonas747 <jonasr747@gmail.com>
2016-08-02 16:10:29 +0200

    Removed GetAllGuildMembers from master

--- dutil.go
+++ dutil.go
@@ -6,24 +6,25 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+// Need mah pr to go through
 // Returns all guild members in a guild
 // It will make `number of members`/1000 requests to the api
-func GetAllGuildMembers(session *discordgo.Session, guilID string) ([]*discordgo.Member, error) {
-	var after string
-	members := make([]*discordgo.Member, 0)
+// func GetAllGuildMembers(session *discordgo.Session, guilID string) ([]*discordgo.Member, error) {
+// 	var after string
+// 	members := make([]*discordgo.Member, 0)
 
-	for {
-		resp, err := session.GuildMembers(guilID, after, 1000)
-		if err != nil {
-			return nil, err
-		}
-		members = append(members, resp...)
+// 	for {
+// 		resp, err := session.GuildMembers(guilID, after, 1000)
+// 		if err != nil {
+// 			return nil, err
+// 		}
+// 		members = append(members, resp...)
 
-		if len(resp) < 1000 {
-			break // Reached the end
-		}
+// 		if len(resp) < 1000 {
+// 			break // Reached the end
+// 		}
 
-		after = members[len(members)-1].User.ID
-	}
-	return members, nil
-}
+// 		after = members[len(members)-1].User.ID
+// 	}
+// 	return members, nil
+// }
