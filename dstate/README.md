7a853d94e7b91ac18f47db4696f5c936f0b49327
jonas747 <jonasr747@gmail.com>
2016-12-28 16:45:49 +0100

    Added stuff and staff and stoff and stiff and uh oh what am i doing

--- dstate/README.md
+++ dstate/README.md
@@ -4,6 +4,40 @@ dstate is an alternative state tracker to the standard one in discordgo.
 
 It's a bit more advanced but offer more features and it's easier to avoid race conditions with.
 
+Example:
+
+Retrieving a channel, and getting the name without data races
+```go
+// Standard state in discrodgo
+
+// call channel, state is rlocked inside
+channel := state.Channel(id)
+// We have to rlock the whole state to get the name
+state.RLock()
+name := channel.Name
+state.RUnlock()
+
+
+
+// dstate
+
+// call channel, state is rlocked inside if lock is set to true
+channelState := state.Channel(true, id)
+// Instead of locking the whole state, we either lock just the channel if it's a private channel, or the parent guild
+channelState.Owner.RLock()
+name := channelstate.Channel.name
+channelState.Owner.RUnlock()
+
+// can also create a copy, which after creation you can access and modify fields without worrying about data races as it's a copy
+channelCopy := channelState.Copy(lock, deep /*also copy perm overwrites*/)
+
+// Some data can be accessed safely without locking as they are never mutated:
+channelState.ID()
+channelState.Type()
+channelState.IsPrivate()
+channelState.Recipient()
+```
+
 Differences:
 
  - Per guild rw mutex
--- dstate/channelstate.go
+++ dstate/channelstate.go
@@ -10,18 +10,66 @@ type ChannelState struct {
 	Owner RWLocker
 	Guild *GuildState
 
+	// These fields are never mutated and can be accessed without locking
+	id        string
+	kind      string
+	recipient *discordgo.User
+	isPrivate bool
+
+	// Accessing the channel without locking the owner yields undefined behaviour
 	Channel  *discordgo.Channel
 	Messages []*MessageState
 }
 
-func (c *ChannelState) Update(newChannel *discordgo.Channel) {
-	if newChannel.PermissionOverwrites == nil {
-		newChannel.PermissionOverwrites = c.Channel.PermissionOverwrites
+func NewChannelState(guild *GuildState, owner RWLocker, channel *discordgo.Channel) *ChannelState {
+
+	// Create a copy of the channel
+	cCopy := copyChannel(channel, true)
+
+	cs := &ChannelState{
+		Owner:   owner,
+		Guild:   guild,
+		Channel: cCopy,
+
+		id:        channel.ID,
+		kind:      channel.Type,
+		isPrivate: channel.IsPrivate,
 	}
-	if newChannel.IsPrivate && newChannel.Recipient == nil {
-		newChannel.Recipient = c.Channel.Recipient
+
+	if channel.IsPrivate && channel.Recipient != nil {
+		// Make a copy of the recipient
+		rec := new(discordgo.User)
+		*rec = *channel.Recipient
+		cs.recipient = rec
 	}
-	*c.Channel = *newChannel
+
+	return cs
+}
+
+// Set of accessors below to access the immutable fields and make sure you can't modify them
+
+// ID returns the channels id
+// This does no locking as ID is immutable
+func (cs *ChannelState) ID() string {
+	return cs.id
+}
+
+// Type returns the channels type
+// This does no locking as Type is immutable
+func (cs *ChannelState) Type() string {
+	return cs.kind
+}
+
+// Recipient returns the channels recipient, if you modify this you get undefined behaviour
+// This does no locking as Recipient is immutable
+func (cs *ChannelState) Recipient() *discordgo.User {
+	return cs.recipient
+}
+
+// IsPrivate returns true if the channel is private
+// This does no locking as IsPrivate is immutable
+func (cs *ChannelState) IsPrivate() bool {
+	return cs.isPrivate
 }
 
 // Copy returns a copy of the channel
@@ -32,14 +80,18 @@ func (c *ChannelState) Copy(lock bool, deep bool) *discordgo.Channel {
 		defer c.Owner.RUnlock()
 	}
 
+	return copyChannel(c.Channel, deep)
+}
+
+func copyChannel(in *discordgo.Channel, deep bool) *discordgo.Channel {
 	cCopy := new(discordgo.Channel)
-	*cCopy = *c.Channel
+	*cCopy = *in
 
 	cCopy.Messages = nil
 	cCopy.PermissionOverwrites = nil
 
 	if deep {
-		for _, pow := range c.Channel.PermissionOverwrites {
+		for _, pow := range in.PermissionOverwrites {
 			powCopy := new(discordgo.PermissionOverwrite)
 			*powCopy = *pow
 			cCopy.PermissionOverwrites = append(cCopy.PermissionOverwrites, pow)
@@ -49,7 +101,32 @@ func (c *ChannelState) Copy(lock bool, deep bool) *discordgo.Channel {
 	return cCopy
 }
 
-func (c *ChannelState) Message(mID string) *MessageState {
+// Update updates a channel
+// Undefined behaviour if owner (guild or state) is not locked
+func (c *ChannelState) Update(lock bool, newChannel *discordgo.Channel) {
+	if lock {
+		c.Owner.Lock()
+		defer c.Owner.Unlock()
+	}
+
+	if newChannel.PermissionOverwrites == nil {
+		newChannel.PermissionOverwrites = c.Channel.PermissionOverwrites
+	}
+	if newChannel.IsPrivate && newChannel.Recipient == nil {
+		newChannel.Recipient = c.Channel.Recipient
+	}
+
+	*c.Channel = *newChannel
+}
+
+// Message returns a message by id or nil if none found
+// The only field safe to query on a message without locking the owner (guild or state) is ID
+func (c *ChannelState) Message(lock bool, mID string) *MessageState {
+	if lock {
+		c.Owner.RLock()
+		defer c.Owner.RUnlock()
+	}
+
 	for _, m := range c.Messages {
 		if m.Message.ID == mID {
 			return m
@@ -59,9 +136,14 @@ func (c *ChannelState) Message(mID string) *MessageState {
 	return nil
 }
 
-func (c *ChannelState) MessageAddUpdate(msg *discordgo.Message, maxMessages int, maxMessageAge time.Duration) {
+// MessageAddUpdate adds or updates an existing message
+func (c *ChannelState) MessageAddUpdate(lock bool, msg *discordgo.Message, maxMessages int, maxMessageAge time.Duration) {
+	if lock {
+		c.Owner.Lock()
+		defer c.Owner.Unlock()
+	}
 
-	existing := c.Message(msg.ID)
+	existing := c.Message(false, msg.ID)
 	if existing != nil {
 		// Patch the existing message
 		if msg.Content != "" {
@@ -129,7 +211,11 @@ func (c *ChannelState) MessageAddUpdate(msg *discordgo.Message, maxMessages int,
 
 // MessageRemove removes a message from the channelstate
 // If mark is true the the message will just be marked as deleted and not removed from the state
-func (c *ChannelState) MessageRemove(messageID string, mark bool) {
+func (c *ChannelState) MessageRemove(lock bool, messageID string, mark bool) {
+	if lock {
+		c.Owner.Lock()
+		defer c.Owner.RUnlock()
+	}
 
 	for i, ms := range c.Messages {
 		if ms.Message.ID == messageID {
